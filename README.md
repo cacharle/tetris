00441e3328d82e761a45652028441013ab7222d7
Charles <sircharlesaze@gmail.com>
2019-09-18 12:27:13 +0200

    Game state structs, tetris API functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tetris
+
+## Todo
+
+- [ ] Example
--- header.h
+++ header.h
@@ -6,13 +6,31 @@
 
 typedef struct
 {
-        SDL_Window *window;
-            SDL_Renderer *renderer;
-                bool running;
+    int foo;
+} Block;
+
+typedef struct
+{
+    Block **grid;
+    int score;
+} Tetris;
+
+typedef struct
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    bool running;
+    Tetris *tetris;
 } GState;
 
+// graphics.c
 GState *graphics_init(int width, int height);
 void graphics_quit(GState *state);
 void graphics_run(GState *state);
+
+// tetris.c
+Tetris *tetris_init(void);
+void tetris_destroy(Tetris *tetris);
+void tetris_next(Tetris *tetris);
 
 #endif
